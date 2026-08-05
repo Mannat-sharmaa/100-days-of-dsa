49fe8fba41ddab4d97f0f52b4ceb5486e1963a48
Mannat <mannatkumar20059@gmail.com>
2026-08-05 21:11:56 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(),s.end());
-       sort(t.begin(),t.end());
-      if(s==t){
+
+        if(s.size() != t.size())
+            return false;
+
+        unordered_map<char,int> mp;
+
+        for(char ch : s){
+            mp[ch]++;
+        }
+
+        for(char ch : t){
+            mp[ch]--;
+
+            if(mp[ch] < 0)
+                return false;
+        }
+
         return true;
-      }
-      return false;
     }
 };
